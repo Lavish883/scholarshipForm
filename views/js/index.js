d47616cfb5c1082108b61353073855adabee751b
Lavish Kumar <70912602+Lavish883@users.noreply.github.com>
2023-01-05 17:42:25 +0000

    added cropper.js libraby

--- views/js/index.js
+++ views/js/index.js
@@ -1,4 +1,5 @@
-// sends a post request
+var profileImage = document.getElementById('profileImage');
+var cropper;
 
 async function postEmailLink(event) {
     const options = {
@@ -15,13 +16,29 @@ async function postEmailLink(event) {
     alert(data);
 }
 
-function compressImage(event) {
+
+
+
+function intializeCropper(event) {
     const file = event.target.files[0];
     const reader = new FileReader();
 
     reader.addEventListener("load", () => {
         // convert image file to base64 string
-        document.getElementById('preview').setAttribute('src', reader.result);
+        profileImage.setAttribute('src', reader.result);
+        // try to destroy a cropper instance if it exists
+        try {
+            cropper.destroy();
+        } catch (error) {
+            console.log(error);
+        }
+
+        cropper = new Cropper(profileImage, {
+            preview: '#imagePreview',
+        });
+        
+        cropper.setDragMode("move");
+
     }, false);
 
     if (file) {
@@ -31,5 +48,5 @@ function compressImage(event) {
 
 // add event listeners
 document.getElementById('submitForm').addEventListener('click', postEmailLink);
-document.getElementById('personImage').addEventListener('change', compressImage);
+document.getElementById('personImage').addEventListener('change', intializeCropper);
 
