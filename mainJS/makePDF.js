feb85dc6c2d2ae3b67df84a4267f53fec0f8f053
Lavish Kumar <lavishk750@gmail.com>
2023-03-19 23:29:23 -0400

    added dockerfile to run in production

--- mainJS/makePDF.js
+++ mainJS/makePDF.js
@@ -2,7 +2,11 @@ const puppeteer = require('puppeteer')
 
 // runs puppeteer and then returns the pdf of the page
 async function makePDF(formId) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ 
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process', '--no-zygote'],
+    headless: true,  
+    executablePath: process.env.PRODUCTION == true ? process.env.PUPPETEER_EXECUTABLE_PATH: puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
 
   await page.goto(process.env.WEBSITELINK + `pdf/d20248a3e30e22982eed0de6eb0a9152/${formId}`, {waitUntil: 'networkidle0'});
